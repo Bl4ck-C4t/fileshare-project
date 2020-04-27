bc2d7f62e568da1b09fb92ac68162a7a5a0ff011
TheShadow88 <stef4oben88@gmail.com>
2020-04-27 17:00:07 +0300

    First commit

---
+++ src/main/java/org/elsys/fileshare00/SNAPSHOT/jar/FIleshareApplication.java
@@ -0,0 +1,13 @@
+package org.elsys.fileshare00.SNAPSHOT.jar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FIleshareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FIleshareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/elsys/fileshare00/SNAPSHOT/jar/FIleshareApplicationTests.java
@@ -0,0 +1,13 @@
+package org.elsys.fileshare00.SNAPSHOT.jar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FIleshareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
