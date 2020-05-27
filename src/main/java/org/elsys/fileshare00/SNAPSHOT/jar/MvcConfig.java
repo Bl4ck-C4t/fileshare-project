514d89428ec764f194f5431734aa7f182ce1e9f0
Stefan Stefanov <stef4oben88@gmail.com>
2020-05-27 18:58:27 +0300

    try 1

--- src/main/java/org/elsys/fileshare00/SNAPSHOT/jar/MvcConfig.java
+++ src/main/java/org/elsys/fileshare00/SNAPSHOT/jar/MvcConfig.java
@@ -9,11 +9,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class MvcConfig implements WebMvcConfigurer {
 
     public void addViewControllers(ViewControllerRegistry registry) {
-        registry.addViewController("/home").setViewName("home");
-        registry.addViewController("/hello").setViewName("hello");
-        registry.addViewController("/{spring:\\w+}")
-                .setViewName("forward:/");
-        registry.addViewController("/{spring:\\w+}/**")
+        registry.addViewController("/**")
             .setViewName("forward:/");
         
 
