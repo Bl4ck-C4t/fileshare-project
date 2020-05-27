1d786063fc653cbee6166d779d240db6aa49fe96
Stefan Stefanov <stef4oben88@gmail.com>
2020-05-27 14:22:26 +0300

    re revert

--- src/main/java/org/elsys/fileshare00/SNAPSHOT/jar/MvcConfig.java
+++ src/main/java/org/elsys/fileshare00/SNAPSHOT/jar/MvcConfig.java
@@ -11,7 +11,10 @@ public class MvcConfig implements WebMvcConfigurer {
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/home").setViewName("home");
         registry.addViewController("/hello").setViewName("hello");
+        registry.addViewController("/{spring:\\w+}")
+                .setViewName("forward:/");
         
+
     }
 
 }
